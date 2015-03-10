4fd9b03e207b89eb5c9248d994c75866f2e6f901
gogotanaka <mail@tanakakazuki.com>
2015-03-10 16:33:57 -0700

    Implement first

--- lib/nokogiri_with_js.rb
+++ lib/nokogiri_with_js.rb
@@ -1,5 +1,24 @@
 require "nokogiri_with_js/version"
 
+require "nokogiri"
+require "capybara"
+require 'capybara/poltergeist'
+
 module NokogiriWithJs
-  # Your code goes here...
+  def self.scrape(url)
+    #poltergistの設定
+    Capybara.register_driver :poltergeist do |app|
+      Capybara::Poltergeist::Driver.new(app, { js_errors: false, timeout: 1000 }) #追加のオプションはググってくださいw
+    end
+    Capybara.default_selector = :css
+    session = Capybara::Session.new(:poltergeist)
+    #自由にUser-Agent設定してください。
+    session.driver.headers = { 'User-Agent' => "Mozilla/5.0 (Macintosh; Intel Mac OS X)" }
+    session.visit url
+
+    session.execute_script "$('#player_ad').hide();so.addVariable('autostart','true');so.write('mediaspace');"
+    session.execute_script
+
+    page = Nokogiri::HTML.parse(session.html)
+  end
 end
